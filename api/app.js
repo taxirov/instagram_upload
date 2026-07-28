58b3eef2b50c33e559304835c322c7710b7901ac
Saad Takhir <118287232+taxirov@users.noreply.github.com>
2026-07-28 09:59:12 +0500

    Change app.html to _app.html in file read

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const { requireAuth } = require('./_auth');
 
 module.exports = function handler(req, res) {
   if (!requireAuth(req, res)) return;
-  const html = fs.readFileSync(path.join(__dirname, 'app.html'), 'utf8');
+  const html = fs.readFileSync(path.join(__dirname, '_app.html'), 'utf8');
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.status(200).send(html);
 };
